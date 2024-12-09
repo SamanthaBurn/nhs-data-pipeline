629d918c545de16aa77b23063c8cafbfc943d64f
L1RH0XHAJ <lirhoxhaj35@gmail.com>
2024-12-09 17:27:07 +0000

    Changes to extraction scripts, and build scripts. Added exploration script

---
+++ scripts/explore.py
@@ -0,0 +1,8 @@
+##########################################
+
+# This python script will be used for exploratory data analysis
+
+##########################################
+
+WILL READ FINAL FILE "data/supporting_facilities.csv"
+
